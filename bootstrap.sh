30ecd4672336085b55cda2863d01e6eb5ca5aa7b
Felix Boenke <felixboenke@Felixs-MBP.home>
2021-12-21 22:12:41 +0100

    chore: init

---
+++ bootstrap.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+#Install XCode
+xcode-select --install
+
+## Install brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+## Install ansible
+brew install ansible
+
+## pull ansible
+# sudo ansible-pull -U https://github.com/FLoppix/ansible.git
